9517e947a56937220a85ff85226cdc0e621dc395
Muhammad Talha Amir <tellyamir16@gmail.com>
2025-09-02 21:40:26 +0500

    incrased sizes of svg

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -151,7 +151,7 @@ export default function Navbar({ activePage }) {
             {/* Hamburger */}
             {/* Hamburger */}
             <button onClick={() => setMenuOpen(true)}>
-              <MenuIcon size={32} color={activePage === "home" ? "#ffffff" : "#000000"} />
+              <MenuIcon size={37} color={activePage === "home" ? "#ffffff" : "#000000"} />
             </button>
 
           </div>
@@ -178,7 +178,7 @@ export default function Navbar({ activePage }) {
 
             {/* Close button on right */}
             <button onClick={() => setMenuOpen(false)}>
-              <CloseIcon size={32} color="#ffffff" />
+              <CloseIcon size={37} color="#ffffff" />
             </button>
 
           </div>
