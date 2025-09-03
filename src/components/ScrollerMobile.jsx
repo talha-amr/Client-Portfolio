9d8118da881d9d4fe9a42797fce63fbc4b839b79
Muhammad Talha Amir <tellyamir16@gmail.com>
2025-09-04 01:35:57 +0500

    fixed animation

--- src/components/ScrollerMobile.jsx
+++ src/components/ScrollerMobile.jsx
@@ -47,7 +47,7 @@ const ScrollerMobile = () => {
                     scrollTrigger: {
                         trigger: card,
                         start: "top 80%",
-                        end: "top 20%",
+                        end: "top 30%",
                         scrub: 1,
                         markers: false,
                     }
